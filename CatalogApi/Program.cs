b0f644b3a258161a66ff5db1a1550ae84c71c0c1
Yael Chavez <you400@gmail.com>
2024-04-09 19:32:33 -0600

    async

--- CatalogApi/Program.cs
+++ CatalogApi/Program.cs
@@ -1,4 +1,7 @@
 
+using CatalogApi.Data;
+using CatalogApi.Repositories;
+
 namespace CatalogApi
 {
     public class Program
@@ -8,6 +11,8 @@ namespace CatalogApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ CatalogApi/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using CatalogApi.Data;
+using CatalogApi.Entities;
+using MongoDB.Driver;
+
+namespace CatalogApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            this.catalogContext = catalogContext;
+        }
+        public async Task CreateProduct(Product product)
+         => await catalogContext.Products.InsertOneAsync(product);
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(x => x.Id, id);
+
+            DeleteResult deleteResult = await catalogContext.Products.DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProductById(string id)
+            => await catalogContext
+            .Products
+            .Find(x => x.Id == id)
+            .FirstOrDefaultAsync();
+
+        public async Task<IEnumerable<Product>> GetProducts()
+          => await catalogContext.Products.Find(x => true).ToListAsync();
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await catalogContext.Products
+                 .ReplaceOneAsync(filter: x => x.Id == product.Id, product);
+            
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
