7feb1f6eca9dc8b82475ec04458078effb44d641
Yael Chavez <you400@gmail.com>
2024-04-11 19:56:13 -0600

    sesion 4

---
+++ Ordering.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System.Linq.Expressions;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class GenericRepository<T>(OrderContext orderContext) where T : EntityBase
+    {
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+            => await orderContext
+                                .Set<T>()
+                                .Where(predicate)
+                                .ToListAsync();
+
+        public async Task<IReadOnlyList<T>> GetAsync(int offset, int limit,
+            Expression<Func<T, bool>>? predicate, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+            params string[] includeString)
+        {
+
+
+        }
+    }
+}
