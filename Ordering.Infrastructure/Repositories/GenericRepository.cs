022f74cb4d89806a7d5ed796b3268a774aea8649
Yael Chavez <you400@gmail.com>
2024-04-12 19:18:27 -0600

    clase 5

--- Ordering.Infrastructure/Repositories/GenericRepository.cs
+++ Ordering.Infrastructure/Repositories/GenericRepository.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts;
 using Ordering.Domain.Common;
 using Ordering.Infrastructure.Persistence;
 using System.Linq.Expressions;
 
 namespace Ordering.Infrastructure.Repositories
 {
-    public class GenericRepository<T>(OrderContext orderContext) where T : EntityBase
+    public class GenericRepository<T>(OrderContext orderContext) : 
+        IGenericRepository<T> where T : EntityBase
     {
         public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
             => await orderContext
@@ -17,8 +19,33 @@ namespace Ordering.Infrastructure.Repositories
             Expression<Func<T, bool>>? predicate, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
             params string[] includeString)
         {
+            IQueryable<T> query = orderContext.Set<T>();
 
+            query.Skip(offset).Take(limit);
 
+            query = includeString.Aggregate(query, (current, itemInclude) => current.Include(itemInclude));
+
+            if (predicate is not null)
+                query = query.Where(predicate);
+
+            if (orderBy is not null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+
+        }
+
+        public async Task<T> AddAsync(T item)
+        {
+            await orderContext.AddAsync(item);
+            await orderContext.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task UpdateAsync(T item)
+        {
+            orderContext.Update(item);
+            await orderContext.SaveChangesAsync();
         }
     }
 }
