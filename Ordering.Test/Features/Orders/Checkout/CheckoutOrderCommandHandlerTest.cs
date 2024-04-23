438a41ce80387a4924db0abbd549ac61618ab579
Yael Chavez <you400@gmail.com>
2024-04-22 18:41:00 -0600

    unit test

---
+++ Ordering.Test/Features/Orders/Checkout/CheckoutOrderCommandHandlerTest.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Moq;
+using Ordering.Application.Contracts;
+using Ordering.Application.Features.Commands.Checkout;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Test.Features.Orders.Checkout
+{
+    public class CheckoutOrderCommandHandlerTest
+    {
+        private readonly CheckoutOrderCommandHandler _handler;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IGenericRepository<Order>> _repository;
+
+        public CheckoutOrderCommandHandlerTest()
+        {
+            _mapper = new Mock<IMapper>();
+            _repository = new Mock<IGenericRepository<Order>>();
+            _handler = new CheckoutOrderCommandHandler(_repository.Object,_mapper.Object);
+        }
+
+
+        [Theory]
+        [InlineData(100000)]
+        [InlineData(12)]
+        [InlineData(100)]
+        public async Task Handle_ShouldAddNewOrderToRepository(int id) 
+        {
+            //Arrange
+            var command = new CheckoutOrderCommand
+            {
+                Address = "hola@hola.com",
+                FirstName = "Yael",
+                LastName = "Chavez",
+                PaymentMethod = 1,
+                TotalPrice = 100,
+                UserName = "ychavez"
+            };
+
+            var orderEntity = new Order { Id = id };
+            _mapper.Setup(x => x.Map<Order>(command)).Returns(orderEntity);
+            _repository.Setup(x => x.AddAsync(orderEntity)).ReturnsAsync(orderEntity);
+
+            //Act
+            var result = await _handler.Handle(command, default);
+          
+
+            //Assert
+            Assert.Equal(result,orderEntity.Id); //verificamos que el resultado sea el ID
+            _repository.Verify(x=> x.AddAsync(orderEntity), Times.Once); // verificamos que se haya ejecutado el AddAsync del repositorio
+            _mapper.Verify(x=> x.Map<Order>(command), Times.Once);
+        
+        }
+    }
+}
---
+++ Ordering.Test/Features/Orders/Checkout/GetOrdersListQueryHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Moq;
+using Ordering.Application.Contracts;
+using Ordering.Application.Features.Queries;
+using Ordering.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Ordering.Test.Features.Orders.Checkout
+{
+    public class GetOrdersListQueryHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ValidUserName_ReturnsOrdersViewModelList()
+        {
+            // Arrange
+            var userName = "testUser";
+            var orders = new List<Order>
+            {
+                new Order { Id = 1, UserName = userName },
+                new Order { Id = 2, UserName = userName }
+            };
+
+            var mockRepository = new Mock<IGenericRepository<Order>>();
+            mockRepository.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Order, bool>>>()))
+                          .ReturnsAsync(orders);
+
+            var mockMapper = new Mock<IMapper>();
+            mockMapper.Setup(mapper => mapper.Map<List<OrdersViewModel>>(orders))
+                      .Returns(new List<OrdersViewModel>
+                      {
+                          new OrdersViewModel { Id = 1, UserName = userName },
+                          new OrdersViewModel { Id = 2, UserName = userName }
+                      });
+
+            var query = new GetOrdersListQuery { UserName = userName };
+            var handler = new GetOrdersListQueryHandler(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<OrdersViewModel>>(result);
+            Assert.Equal(2, result.Count);
+        }
+    }
+}
